533f266cfe9fb07fb1c73717ec1943df4719b630
YsfJbrSimplelab <yj@simplelab.info>
2022-03-13 22:13:07 +0100

    correction mx

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Mon super site</title>
+    <!-- Methode Gregory :
+    <link rel="stylesheet" href="{{ url(mix('css/app.css')) }}">
+    -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,60 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        './app/**/*.php',
+        './resources/**/*.html',
+        './resources/**/*.js',
+        './resources/**/*.jsx',
+        './resources/**/*.ts',
+        './resources/**/*.tsx',
+        './resources/**/*.php',
+        './resources/**/*.vue',
+        './resources/**/*.twig',
+    ],
+    options: {
+        defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
+        whitelistPatterns: [/-active$/, /-enter$/, /-leave-to$/, /show$/],
+    },
+    theme: {
+        colors: {
+            inherit: colors.inherit,
+            current: colors.current,
+            transparent: colors.transparent,
+            black: colors.black,
+            white: colors.white,
+            slate: colors.slate,
+            gray: colors.gray,
+            zinc: colors.zinc,
+            neutral: colors.neutral,
+            stone: colors.stone,
+            red: colors.red,
+            orange: colors.orange,
+            amber: colors.amber,
+            yellow: colors.yellow,
+            lime: colors.lime,
+            green: colors.green,
+            emerald: colors.emerald,
+            teal: colors.teal,
+            cyan: colors.cyan,
+            sky: colors.sky,
+            blue: colors.blue,
+            indigo: colors.indigo,
+            violet: colors.violet,
+            purple: colors.purple,
+            fuchsia: colors.fuchsia,
+            pink: colors.pink,
+            rose: colors.rose,
+        },
+    },
+    variants: {
+        extend: {
+            backgroundColor: ['active'],
+        }
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/aspect-ratio')
+    ],
 }
